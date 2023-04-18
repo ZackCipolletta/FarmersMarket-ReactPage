eff9ab31efa3c57cba115d61d7bb8e83d0a5d79e
Zachary Cipolletta <Zack.Cipolletta@gmail.com>
2023-04-18 15:13:10 -0700

    Create app that displays the schedule and selected offerings for a farmer's market depending on the day of week and month of year.
    Co-Authored-by: Brent Hickman<brenthickman@ymail.com>

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import DatePicker from './DatePicker';
+import Schedule from './Schedule';
+import ProduceList from './ProduceList';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <DatePicker />
+
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Schedule.js
@@ -0,0 +1,53 @@
+import React from "react";
+import DatePicker from "./DatePicker";
+
+const marketSchedule = [
+  {
+    day: "Sunday",
+    location: "Lents International",
+    hours: "9:00am - 2:00pm",
+    booth: "4A"
+  },
+  {
+    day: "Monday",
+    location: "Pioneer Courthouse Square",
+    hours: "10:00am - 2:00pm",
+    booth: "7C"
+  },
+  {
+    day: "Tuesday",
+    location: "Hillsboro",
+    hours: "5:00pm - 8:30pm",
+    booth: "1F"
+  },
+  {
+    day: "Wednesday",
+    location: "Shemanski Park",
+    hours: "10:00am - 2:00pm",
+    booth: "3E"
+  },
+  {
+    day: "Thursday",
+    location: "Northwest Portland",
+    hours: "2:00pm - 6:00pm",
+    booth: "6D"
+  },
+  {
+    day: "Saturday",
+    location: "Beaverton",
+    hours: "10:00am - 1:30pm",
+    booth: "9G"
+  }
+];
+
+function Schedule(props) {
+  return (
+    <React.Fragment>
+      <h3>Farmers Market Schedule for {marketSchedule[props.selectedDay].day}s</h3>
+      <p>Come see on  {marketSchedule[props.selectedDay].day}s at {marketSchedule[props.selectedDay].location} at booth {marketSchedule[props.selectedDay].booth}.  We are open {marketSchedule[props.selectedDay].hours}</p>
+      {marketSchedule[props.selectedDay].selection}
+    </React.Fragment>
+  );
+}
+
+export default Schedule;
